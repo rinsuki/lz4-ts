e97b14716f60abcc51178abec35241881dbe0d81
rinsuki <428rinsuki+git@gmail.com>
2020-06-13 19:58:29 +0900

    many something

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-// lz4-ts @license BSD-3-Clause / Copyright (c) 2015, Pierre Curto / 2016, oov. All rights reserved.
+// lz4-ts @license BSD-3-Clause / Copyright (c) 2015, Pierre Curto / 2016, oov / 2020, rinsuki. All rights reserved.
 /**
  * Copyright (c) 2015, Pierre Curto
  * Copyright (c) 2016, oov
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
- * 
+ *
  * * Neither the name of xxHash nor the names of its
  *   contributors may be used to endorse or promote products derived from
  *   this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -30,48 +30,40 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-const errInvalidSource = new Error('invalid source');
-const errShortBuffer = new Error('short buffer');
+const errInvalidSource = new Error("invalid source");
+const errShortBuffer = new Error("short buffer");
 
 // The following constants are used to setup the compression algorithm.
-const minMatch   = 4;  // the minimum size of the match sequence size (4 bytes)
+const minMatch = 4; // the minimum size of the match sequence size (4 bytes)
 const winSizeLog = 16; // LZ4 64Kb window size limit
-const winSize    = 1 << winSizeLog;
-const winMask    = winSize - 1; // 64Kb window of previous data for dependent blocks
+const winSize = 1 << winSizeLog;
+const winMask = winSize - 1; // 64Kb window of previous data for dependent blocks
 
 // hashSizeLog determines the size of the hash table used to quickly find a previous match position.
 // Its value influences the compression speed and memory usage, the lower the faster,
 // but at the expense of the compression ratio.
 // 16 seems to be the best compromise.
-const hashSizeLog   = 16;
-const hashSize      = 1 << hashSizeLog;
-const hashShift     = (minMatch * 8) - hashSizeLog;
+const hashSizeLog = 16;
+const hashSize = 1 << hashSizeLog;
+const hashShift = minMatch * 8 - hashSizeLog;
 
-const mfLimit      = 8 + minMatch; // The last match cannot start within the last 12 bytes.
-const skipStrength = 6;            // variable step for fast scan
+const mfLimit = 8 + minMatch; // The last match cannot start within the last 12 bytes.
+const skipStrength = 6; // variable step for fast scan
 
 const hasher = 2654435761 | 0; // prime number used to hash minMatch
 
-// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Math/imul#Polyfill
-function imulPolyfill(a: number, b: number): number {
-    const ah  = (a >>> 16) & 0xffff;
-    const al = a & 0xffff;
-    const bh  = (b >>> 16) & 0xffff;
-    const bl = b & 0xffff;
-    // the shift by 0 fixes the sign on the high part
-    // the final |0 converts the unsigned value into a signed value
-    return al * bl + (((ah * bl + al * bh) << 16) >>> 0) | 0;
-};
-const imul = Math.imul ? Math.imul : imulPolyfill;
-
 function getUint32(a: Uint8Array, i: number): number {
-    return (a[i + 3]) | (a[i + 2] << 8) | (a[i + 1] << 16) | (a[i] << 24);
+    return a[i + 3] | (a[i + 2] << 8) | (a[i + 1] << 16) | (a[i] << 24);
 }
 
-function copy(dest: Uint8Array, src: Uint8Array, di: number, si: number, len: number): void {
-    for (let i = 0; i < len; ++i) {
-        dest[di++] = src[si++];
-    }
+function copy(
+    dest: Uint8Array,
+    src: Uint8Array,
+    di: number,
+    si: number,
+    len: number
+): void {
+    dest.set(src.subarray(si, len), di);
 }
 
 export function calcUncompressedLen(src: Uint8Array): number {
@@ -221,16 +213,21 @@ export function uncompressBlock(src: Uint8Array, dest: Uint8Array): number {
 }
 
 export function compressBlockBound(n: number): number {
-    return n + (n / 255 | 0) + 16;
+    return n + ((n / 255) | 0) + 16;
 }
 
-export function compressBlock(src: Uint8Array, dest: Uint8Array, soffset: number): number {
+export function compressBlock(
+    src: Uint8Array,
+    dest: Uint8Array,
+    soffset: number
+): number {
     const sn = src.length - mfLimit;
     const dn = dest.length;
     if (sn <= 0 || dn === 0 || soffset >= sn) {
         return 0;
     }
-    let si = 0, di = 0;
+    let si = 0,
+        di = 0;
 
     // fast scan strategy:
     // we only need a hash table to store the last sequences (4 bytes)
@@ -239,7 +236,7 @@ export function compressBlock(src: Uint8Array, dest: Uint8Array, soffset: number
     // Initialise the hash table with the first 64Kb of the input buffer
     // (used when compressing dependent blocks)
     while (si < soffset) {
-        const h = imul(getUint32(src, si), hasher) >>> hashShift;
+        const h = Math.imul(getUint32(src, si), hasher) >>> hashShift;
         hashTable[h] = ++si;
     }
 
@@ -247,7 +244,7 @@ export function compressBlock(src: Uint8Array, dest: Uint8Array, soffset: number
     let fma = 1 << skipStrength;
     while (si < sn - minMatch) {
         // hash the next 4 bytes (sequence)...
-        const h = imul(getUint32(src, si), hasher) >>> hashShift;
+        const h = Math.imul(getUint32(src, si), hasher) >>> hashShift;
         // -1 to separate existing entries from new ones
         const ref = hashTable[h] - 1;
         // ...and store the position of the hash in the hash table (+1 to compensate the -1 upon saving)
@@ -263,12 +260,14 @@ export function compressBlock(src: Uint8Array, dest: Uint8Array, soffset: number
         // => so overlap must be 0
 
         // the sequence is new, out of bound (64kb) or not valid: try next sequence
-        if (ref < 0 || //(fma & ((1 << skipStrength) - 1)) < 4 || // this code seems has a big penalty for size...
+        if (
+            ref < 0 || //(fma & ((1 << skipStrength) - 1)) < 4 || // this code seems has a big penalty for size...
             (si - ref) >> winSizeLog > 0 ||
             src[ref] !== src[si] ||
             src[ref + 1] !== src[si + 1] ||
             src[ref + 2] !== src[si + 2] ||
-            src[ref + 3] !== src[si + 3]) {
+            src[ref + 3] !== src[si + 3]
+        ) {
             // variable step: improves performance on non-compressible data
             si += fma >> skipStrength;
             ++fma;
@@ -384,13 +383,18 @@ export function compressBlock(src: Uint8Array, dest: Uint8Array, soffset: number
     return di;
 }
 
-export function compressBlockHC(src: Uint8Array, dest: Uint8Array, soffset: number): number {
+export function compressBlockHC(
+    src: Uint8Array,
+    dest: Uint8Array,
+    soffset: number
+): number {
     const sn = src.length - mfLimit;
     const dn = dest.length;
     if (sn <= 0 || dn === 0 || soffset >= sn) {
         return 0;
     }
-    let si = 0, di = 0;
+    let si = 0,
+        di = 0;
 
     // Hash Chain strategy:
     // we need a hash table and a chain table
@@ -401,7 +405,7 @@ export function compressBlockHC(src: Uint8Array, dest: Uint8Array, soffset: numb
     // Initialise the hash table with the first 64Kb of the input buffer
     // (used when compressing dependent blocks)
     while (si < soffset) {
-        const h = imul(getUint32(src, si), hasher) >>> hashShift;
+        const h = Math.imul(getUint32(src, si), hasher) >>> hashShift;
         chainTable[si & winMask] = hashTable[h];
         hashTable[h] = ++si;
     }
@@ -409,12 +413,16 @@ export function compressBlockHC(src: Uint8Array, dest: Uint8Array, soffset: numb
     let anchor = si;
     while (si < sn - minMatch) {
         // hash the next 4 bytes (sequence)...
-        const h = imul(getUint32(src, si), hasher) >>> hashShift;
+        const h = Math.imul(getUint32(src, si), hasher) >>> hashShift;
 
         // follow the chain until out of window and give the longest match
         let mLen = 0;
         let offset = 0;
-        for (let next = hashTable[h] - 1; next > 0 && next > si - winSize; next = chainTable[next & winMask] - 1) {
+        for (
+            let next = hashTable[h] - 1;
+            next > 0 && next > si - winSize;
+            next = chainTable[next & winMask] - 1
+        ) {
             // the first (mLen==0) or next byte (mLen>=minMatch) at current match length must match to improve on the match length
             if (src[next + mLen] === src[si + mLen]) {
                 for (let ml = 0; ; ++ml) {
@@ -442,7 +450,7 @@ export function compressBlockHC(src: Uint8Array, dest: Uint8Array, soffset: numb
         // update hash/chain tables with overlaping bytes:
         // si already hashed, add everything from si+1 up to the match length
         for (let si2 = si + 1, ml = si + mLen; si2 < ml; ) {
-            const h = imul(getUint32(src, si2), hasher) >>> hashShift;
+            const h = Math.imul(getUint32(src, si2), hasher) >>> hashShift;
             chainTable[si2 & winMask] = hashTable[h];
             hashTable[h] = ++si2;
         }
